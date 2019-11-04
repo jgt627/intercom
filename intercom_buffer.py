aab2c6dce72efedbf57f88e2b71c39fbaabc978d
jgt627 <55431742+jgt627@users.noreply.github.com>
2019-11-04 21:53:46 +0100

    Add files via upload

--- intercom_buffer.py
+++ intercom_buffer.py
@@ -1,69 +1,61 @@
+# Adding a buffer.
+
 import sounddevice as sd
-import struct                                        # https://python-sounddevice.readthedocs.io
-import numpy                                                                    # https://numpy.org/
-import argparse                                                                 # https://docs.python.org/3/library/argparse.html
-import socket                                                                   # https://docs.python.org/3/library/socket.html
-import queue                                                                    # https://docs.python.org/3/library/queue.html
-import sys
+import numpy as np
+import struct
 from intercom import Intercom
+
+if __debug__:
+    import sys
 
 class Intercom_buffer(Intercom):
 
+    MAX_CHUNK_NUMBER = 65536
+
     def init(self, args):
         Intercom.init(self, args)
-        self.cl = 0
-        self.ce = 1
-        self.l = [None]*64
+        self.chunks_to_buffer = args.chunks_to_buffer
+        self.cells_in_buffer = self.chunks_to_buffer * 2
+        self._buffer = [self.generate_zero_chunk()] * self.cells_in_buffer
+        self.packet_format = f"HHH{self.samples_per_chunk}h"
+        if __debug__:
+            print(f"chunks_to_buffer={self.chunks_to_buffer}")
+
     def run(self):
-        sending_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        receiving_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        listening_endpoint = ("0.0.0.0", self.listening_port)
-        receiving_sock.bind(listening_endpoint)
+
+        self.recorded_chunk_number = 0
+        self.played_chunk_number = 0
 
         def receive_and_buffer():
-            estructura, source_address = receiving_sock.recvfrom(Intercom.max_packet_size)
+            message, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
+            chunk_number, *chunk = struct.unpack(self.packet_format, message)
+            self._buffer[chunk_number % self.cells_in_buffer] = np.asarray(chunk).reshape(self.frames_per_chunk, self.number_of_channels)
+            return chunk_number
 
-            tupla = struct.unpack('!%shh' % 2048, estructura)
-
-            cont = tupla[len(tupla)-1]          #obtiene el contador
-            a = []                              #transformar la tupla en un ndarray bidimensional
-            for x in range(0, 2047, 2):
-                tuplaux = (numpy.int16(tupla[x]), numpy.int16(tupla[x+1]))
-                a.append(numpy.asarray(tuplaux))
-            b = numpy.asarray(a)
-            
-            
-            self.ce += 1
-            self.ce %= 64
-            self.l[cont] = b
         def record_send_and_play(indata, outdata, frames, time, status):
-            
-            
-            estructura = struct.pack('!%shh' % (indata.size), *indata.flatten('C'), self.ce)
+            print(type(indata))
 
-            sending_sock.sendto(estructura, (self.destination_IP_addr, self.destination_port))
-
-
-            if numpy.all(self.l[self.cl] != None):
-                message = self.l[self.cl]
-                self.l[self.cl] = None
-               
-            elif self.l[self.cl] == None: 
-                message = numpy.zeros((self.samples_per_chunk, self.number_of_channels), self.dtype)
-            
-            outdata[:] = numpy.frombuffer(message, numpy.int16).reshape(self.samples_per_chunk, self.number_of_channels)
-            self.cl += 1
-            self.cl %= 64
-
+            message = struct.pack(self.packet_format, self.recorded_chunk_number, *(indata.flatten()))
+            self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
+            self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
+            chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
+            self._buffer[self.played_chunk_number % self.cells_in_buffer] = self.generate_zero_chunk()
+            self.played_chunk_number = (self.played_chunk_number + 1) % self.cells_in_buffer
+            outdata[:] = chunk
             if __debug__:
-                sys.stderr.write(".")
-                sys.stderr.flush()
+                sys.stderr.write("."); sys.stderr.flush()
 
-        with sd.Stream(samplerate=self.samples_per_second, blocksize=self.samples_per_chunk, dtype=self.dtype, channels=self.number_of_channels, callback=record_send_and_play):
-            print('-=- Press <CTRL> + <C> to quit -=-')
+        with sd.Stream(samplerate=self.frames_per_second, blocksize=self.frames_per_chunk, dtype=np.int16, channels=self.number_of_channels, callback=record_send_and_play):
+            print("-=- Press CTRL + c to quit -=-")
+            first_received_chunk_number = receive_and_buffer()
+            self.played_chunk_number = (first_received_chunk_number - self.chunks_to_buffer) % self.cells_in_buffer
             while True:
                 receive_and_buffer()
 
+    def add_args(self):
+        parser = Intercom.add_args(self)
+        parser.add_argument("-cb", "--chunks_to_buffer", help="Number of chunks to buffer", type=int, default=32)
+        return parser
 
 if __name__ == "__main__":
     intercom = Intercom_buffer()
