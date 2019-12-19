337b183c46c71de65a986a1a320ab0bc90008c93
jgt627 <55431742+jgt627@users.noreply.github.com>
2019-12-19 21:48:13 +0100

    Add files via upload

--- intercom.py
+++ intercom.py
@@ -1,3 +1,5 @@
+# A very simple intercom.
+#
 # No video, no DWT, no compression, no bitplanes, no data-flow
 # control, no buffering. Only the transmission of the raw audio data,
 # splitted into chunks of fixed length.
@@ -45,28 +47,29 @@ class Intercom:
             print(f"bytes_per_chunk={self.bytes_per_chunk}")
 
     def generate_zero_chunk(self):
-        return np.zeros((self.frames_per_chunk, self.number_of_channels), np.int16)
-    def run(self):
+        cell = np.zeros((self.frames_per_chunk, self.number_of_channels), np.int16)
+        return cell
 
-        def receive_and_buffer():
-            message, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
-            chunk = np.frombuffer(message, np.int16).reshape(self.frames_per_chunk, self.number_of_channels)
-            self.q.put(chunk)
+    def receive_and_buffer(self):
+        message, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
+        chunk = np.frombuffer(message, np.int16).reshape(self.frames_per_chunk, self.number_of_channels)
+        self.q.put(chunk)
         
-        def record_send_and_play(indata, outdata, frames, time, status):
-            self.sending_sock.sendto(indata, (self.destination_IP_addr, self.destination_port))
-            try:
-                chunk = self.q.get_nowait()
-            except queue.Empty:
-                chunk = self.generate_zero_chunk()
-            outdata[:] = chunk
-            if __debug__:
-                sys.stderr.write("."); sys.stderr.flush()
-
-        with sd.Stream(samplerate=self.frames_per_second, blocksize=self.frames_per_chunk, dtype=np.int16, channels=self.number_of_channels, callback=record_send_and_play):
-            print("-=- Press CTRL + c to quit -=-")
+    def record_send_and_play(self, indata, outdata, frames, time, status):
+        self.sending_sock.sendto(indata, (self.destination_IP_addr, self.destination_port))
+        try:
+            chunk = self.q.get_nowait()
+        except queue.Empty:
+            chunk = self.generate_zero_chunk()
+        outdata[:] = chunk
+        if __debug__:
+            sys.stderr.write("."); sys.stderr.flush()
+            
+    def run(self):
+        with sd.Stream(samplerate=self.frames_per_second, blocksize=self.frames_per_chunk, dtype=np.int16, channels=self.number_of_channels, callback=self.record_send_and_play):
+            print("-=- Press <CTRL> + <c> to quit -=-")
             while True:
-                receive_and_buffer()
+                self.receive_and_buffer()
 
     def add_args(self):
         parser = argparse.ArgumentParser(description="Real-time intercom", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
---
+++ intercom_empty.py
@@ -0,0 +1,54 @@
+# Don't send empty bitplanes.
+#
+# The sender adds to the number of received bitplanes the number of
+# skipped (zero) bitplanes of the chunk sent.
+
+# The receiver computes the first received
+# bitplane (apart from the bitplane with the signs) and report a
+# number of bitplanes received equal to the real number of received
+# bitplanes plus the number of skipped bitplanes.
+
+import struct
+import numpy as np
+from intercom import Intercom
+from intercom_dfc import Intercom_DFC
+
+if __debug__:
+    import sys
+
+class Intercom_empty(Intercom_DFC):
+
+    def init(self, args):
+        Intercom_DFC.init(self, args)
+        self.ignored_bps = 0
+        self.packet_format = f"!BHBB{self.frames_per_chunk//8}B"
+
+    def receive_and_buffer(self):
+        
+        message, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
+        ig_bps, received_chunk_number, received_bitplane_number, self.NORB, *bitplane = struct.unpack(self.packet_format, message)
+        bitplane = np.asarray(bitplane, dtype=np.uint8)
+        bitplane = np.unpackbits(bitplane)
+        bitplane = bitplane.astype(np.uint16)
+        self._buffer[received_chunk_number % self.cells_in_buffer][:, received_bitplane_number%self.number_of_channels] |= (bitplane << received_bitplane_number//self.number_of_channels)
+        self.received_bitplanes_per_chunk[received_chunk_number % self.cells_in_buffer] += 1 + ig_bps
+        return received_chunk_number
+    
+    def send_bitplane(self, indata, bitplane_number):        
+        bitplane = (indata[:, bitplane_number%self.number_of_channels] >> bitplane_number//self.number_of_channels) & 1
+        if not np.any(bitplane):
+            self.ignored_bps += 1
+        else:
+            bitplane = bitplane.astype(np.uint8)
+            bitplane = np.packbits(bitplane)
+            message = struct.pack(self.packet_format, self.ignored_bps, self.recorded_chunk_number, bitplane_number, self.received_bitplanes_per_chunk[(self.played_chunk_number+1) % self.cells_in_buffer]+1, *bitplane)
+            self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
+            self.ignored_bps = 0
+    
+
+if __name__ == "__main__":
+    intercom = Intercom_empty()
+    parser = intercom.add_args()
+    args = parser.parse_args()
+    intercom.init(args)
+    intercom.run()
